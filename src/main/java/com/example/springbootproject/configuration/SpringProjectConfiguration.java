f8676a171a38f2a4e013f4ba1fd23c0b30042759
Kateryna-Chernovol <schasliva@yahoo.com>
2022-02-06 17:59:14 +0200

    Draft Commit

--- src/main/java/com/example/springbootproject/configuration/SpringProjectConfiguration.java
+++ src/main/java/com/example/springbootproject/configuration/SpringProjectConfiguration.java
@@ -3,6 +3,7 @@ package com.example.springbootproject.configuration;
 import com.example.springbootproject.logger.DevLogger;
 import com.example.springbootproject.logger.Logger;
 import com.example.springbootproject.logger.ProdLogger;
+import com.example.springbootproject.logger.TestLogger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +12,17 @@ import org.springframework.context.annotation.Configuration;
 public class SpringProjectConfiguration {
 
     @Value("${environment}")
+//    @Value("test")
     private String env;
 
     @Bean
     public Logger logger() {
-        if("dev".equals(env)) {
+        if ("dev".equals(env)) {
             return new DevLogger();
-        } else {
+        } else if ("prod".equals(env)) {
             return new ProdLogger();
+        } else {
+            return new TestLogger();
         }
     }
 }
